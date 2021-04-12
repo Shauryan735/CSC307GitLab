40038df722fe78076b597bf8ac8dc2d3c17a7fa8
Shauryan735 <70450436+Shauryan735@users.noreply.github.com>
2021-04-12 15:21:40 -0700

    Added years at Cal Poly to Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
    public static void main(String args[]) {
-      System.out.println("Hello World");
+      System.out.println("Hello World. I have been in Cal Poly for almost 2 years");
    }
 
 }
